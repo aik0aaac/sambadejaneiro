f69da429c66527880c89a05892428d9ee9e8bb6c
Saika Ura <aik0aaad@gmail.com>
2019-09-09 18:25:20 +0900

    好きな顔文字を1つだけ一緒に踊らせることができる機能追加

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # これはなーに
 かの有名な[サンバを踊るCSS](https://github.com/aik0aaac/sambadejaneiro)を元にちょっと機能を追加してみました。
 [YuichiSemura](https://github.com/YuichiSemura)さんごめんなさい。
+
+こんな感じで好きな顔文字を一緒に踊らせることができます。
+![プレビュー](https://imgur.com/PJQzyJh)
--- samba.js
+++ samba.js
@@ -1,84 +1,144 @@
-window.addEventListener("keydown", handleKeydown);
-window.addEventListener("keyup", handleKeyup);
-var sambaNow = false;
-var enterFlg = false;
-var ctrFlg = false;
-function handleKeydown(event){
-    if(event.key === 'Enter'){
-        enterFlg = true;
-    }else if(event.key === 'Control'){
-        ctrFlg = true;
-    }
-    if(!enterFlg || !ctrFlg) return;
-    if(sambaNow){
-        stop();
-    }else{
-        start();
-    }
-    sambaNow = !sambaNow;
+// 入力キー(Enter, Ctrl)のイベント受け取り
+window.addEventListener('keydown', handleKeydown);
+window.addEventListener('keyup', handleKeyup);
+
+// 各種フラグ系
+let sambaNow = false;
+let enterFlg = false;
+let ctrFlg = false;
+
+/**
+ * キーが押された時の処理
+ * @param {*} event 
+ */
+function handleKeydown(event) {
+  if (event.key === 'Enter') {
+    enterFlg = true;
+  } else if (event.key === 'Control') {
+    ctrFlg = true;
+  }
+  if (!enterFlg || !ctrFlg) {
+    return;
+  }
+  if (sambaNow) {
+    sambaStop();
+  } else {
+    sambaStart();
+  }
+  sambaNow = !sambaNow;
 }
 
-function handleKeyup(event){
-    if(event.key === 'Enter'){
-        enterFlg = false;
-    }else if(event.key === 'Control'){
-        ctrFlg = false;
-    }
+/**
+ * キーが離れた時の処理
+ * @param {*} event 
+ */
+function handleKeyup(event) {
+  if (event.key === 'Enter') {
+    enterFlg = false;
+  } else if (event.key === 'Control') {
+    ctrFlg = false;
+  }
 }
 
-function start(){
-    var tateList1 = document.getElementsByClassName("left");
-    for(var i = 0; i < tateList1.length; i++) {
-        tateList1[i].classList.add('hurueruLeft');
+// sambaの子コンポーネント(データが格納される箱)を作成
+const SambaPerson = {
+  props: ['mouth'],
+  template: '<div class="samba-person">' +
+    '<span class="left-hand">＼</span>' +
+    '<span class="face">({{ mouth }})</span>' +
+    '<span class="right-hand">／</span>' +
+    '</div>'
+};
+
+let sambaApp = new Vue({
+  el: '#sambaApp',
+  data: {
+    customSambaPerson: '',
+    sambaNowDoing: false,
+  },
+  components: {
+    'samba-person-component': SambaPerson,
+  },
+  methods: {
+    addSumbaPerson: function (event) {
+      console.log(this.sambaPersonMouth.val);
     }
-    var tateList2 = document.getElementsByClassName("face");
-    for(var i = 0; i < tateList2.length; i++) {
-        tateList2[i].classList.add('hurueru');
-    }
-    var tateList3 = document.getElementsByClassName("right");
-    for(var i = 0; i < tateList1.length; i++) {
-        tateList3[i].classList.add('hurueruRight');
-    }
-    var rowDiv1 = document.getElementById("Row1");
-    rowDiv1.classList.add('yure1');
-    var rowDiv2 = document.getElementById("Row2");
-    rowDiv2.classList.add('yure2');
-    var rowDiv3 = document.getElementById("Row3");
-    rowDiv3.classList.add('yure1');
-    var all1 = document.getElementById("all1");
-    all1.classList.remove('all');
-    all1.classList.add('all2');
-    document.getElementById("haka").classList.add('hurueru');
-    document.getElementById("message").classList.remove('msg');
-    document.getElementById("message").classList.add('msg07');
+  }
+});
+
+
+
+function sambaStart() {
+  // サンバダンサーズが踊り始める: 手や顔の動き
+  addClassToArrayByClassName('left-hand', 'shake-left');
+  addClassToArrayByClassName('face', 'shake');
+  addClassToArrayByClassName('right-hand', 'shake-right');
+
+  // サンバダンサーズが踊り始める: 横に
+  document.getElementById('Row1').classList.add('shake-type-left');
+  document.getElementById('Row2').classList.add('shake-type-right');
+  document.getElementById('Row3').classList.add('shake-type-left');
+  document.getElementById('RowEx').classList.add('shake-type-right');
+
+  // サンバダンサーズが入場(opacity:1)
+  document.getElementById('samba-people').classList.add('samba-people-show');
+  document.getElementById('samba-people').classList.remove('samba-people-snake');
+
+  // ついでにメッセージと墓も(opacity:1)
+  document.getElementById('haka').classList.add('shake');
+  document.getElementById('message').classList.remove('msg');
+  document.getElementById('message').classList.add('msg07');
 }
 
-function stop(){
-    var tateList1 = document.getElementsByClassName("left");
-    for(var i = 0; i < tateList1.length; i++) {
-        tateList1[i].classList.remove('hurueruLeft');
-    }
-    var tateList2 = document.getElementsByClassName("face");
-    for(var i = 0; i < tateList2.length; i++) {
-        tateList2[i].classList.remove('hurueru');
-    }
-    var tateList3 = document.getElementsByClassName("right");
-    for(var i = 0; i < tateList1.length; i++) {
-        tateList3[i].classList.remove('hurueruRight');
-    }
-    var rowDiv1 = document.getElementById("Row1");
-    rowDiv1.classList.remove('yure1');
-    rowDiv1.classList.add('yurezu');
-    var rowDiv2 = document.getElementById("Row2");
-    rowDiv2.classList.remove('yure2');
-    rowDiv2.classList.add('yurezu');
-    var rowDiv3 = document.getElementById("Row3");
-    rowDiv3.classList.remove('yure1');
-    rowDiv3.classList.add('yurezu');
-    var all1 = document.getElementById("all1");
-    all1.classList.remove('all2');
-    all1.classList.add('all');
-    document.getElementById("haka").classList.remove('hurueru');
-    document.getElementById("message").classList.add('msg');
-    document.getElementById("message").classList.remove('msg07');
+function sambaStop() {
+  // サンバダンサーズが踊りをやめる: 手や顔の動き
+  removeClassToArrayByClassName('left-hand', 'shake-left');
+  removeClassToArrayByClassName('face', 'shake');
+  removeClassToArrayByClassName('right-hand', 'shake-right');
+
+  // サンバダンサーズが踊りをやめる: 横に
+  document.getElementById('Row1').classList.remove('shake-type-left');
+  document.getElementById('Row1').classList.add('not-shake');
+  document.getElementById('Row2').classList.remove('shake-type-right');
+  document.getElementById('Row2').classList.add('not-shake');
+  document.getElementById('Row3').classList.remove('shake-type-left');
+  document.getElementById('Row3').classList.add('not-shake');
+  document.getElementById('RowEx').classList.remove('shake-type-right');
+  document.getElementById('RowEx').classList.add('not-shake');
+
+  // サンバダンサーズが退場(opacity:0)
+  document.getElementById("samba-people").classList.remove('samba-people-show');
+  document.getElementById("samba-people").classList.add('samba-people-snake');
+
+  // ついでにメッセージと墓も(opacity:0)
+  document.getElementById('haka').classList.remove('shake');
+  document.getElementById('message').classList.add('msg');
+  document.getElementById('message').classList.remove('msg07');
+}
+
+
+/**
+ * 指定したクラス名を持つDOM要素に指定したクラス名を追加します。
+ * @param {string} targetClassName 対象のDOM要素が持つクラス名
+ * @param {string} addClassName 対象のDOM要素に追加したいクラス名
+ */
+function addClassToArrayByClassName(targetClassName, addClassName) {
+  let array = document.getElementsByClassName(targetClassName);
+  for (let i = 0; i < array.length; i++) {
+    array[i].classList.add(addClassName);
+  }
+  return;
+}
+
+/**
+ * 指定したクラス名を持つDOM要素から指定したクラス名を消去します。
+ * @param {string} targetClassName 対象のDOM要素が持つクラス名
+ * @param {string} removeClassName 対象のDOM要素から消去したいクラス名
+ */
+function removeClassToArrayByClassName(targetClassName, removeClassName) {
+  let array = document.getElementsByClassName(targetClassName);
+  for (let i = 0; i < array.length; i++) {
+    array[i].classList.remove(removeClassName);
+  }
+  return;
 }
